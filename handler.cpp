43c56fabad1b0c15fc39779455923b30c9f6ed57
F0xedb <tom@odex.be>
2019-09-08 23:05:46 +0200

    Only allow UTF-8 based languages

--- handler.cpp
+++ handler.cpp
@@ -18,10 +18,10 @@ QStringList Handler::getDisks(){
     return stringlist;
 }
 
-// awk '$0 ~ /^#[a-zA-Z].*/' /etc/locale.gen | sed 's:#::' | awk '{print $1}'
+// awk '$0 ~ /^#[a-zA-Z].*\.UTF-8/' /etc/locale.gen | sed 's:#::' | awk '{print $1}'
 QStringList Handler::getLocals(){
     QProcess process;
-    process.start("/bin/bash", QStringList() << "-c" << "awk '$0 ~ /^#[a-zA-Z].*/' /etc/locale.gen | sed 's:#::' | awk '{print $1}'");
+    process.start("/bin/bash", QStringList() << "-c" << "awk '$0 ~ /^#[a-zA-Z].*\\.UTF-8/' /etc/locale.gen | sed 's:#::' | awk '{print $1}'");
     process.waitForFinished();
     QString result = process.readAllStandardOutput();
     QStringList stringlist = result.split("\n");
